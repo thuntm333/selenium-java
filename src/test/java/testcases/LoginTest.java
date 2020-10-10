37d523185f94c4e8b28ed7ce6c68e55292dc4f1f
thuntm333 <mthu.nguyen394@gmail.com>
2020-10-10 10:48:16 +0700

    change to page object model using by

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -1,28 +1,29 @@
 package testcases;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.Login;
 
 public class LoginTest {
     private WebDriver driver;
 
     @BeforeMethod
-    void beforeMethod(){
+    void beforeMethod() {
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/login");
     }
+
     @Test
-    void tc01(){
-        driver.findElement(By.id("username")).sendKeys("tomsmith");
-        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-        driver.findElement(By.xpath("//button[@type='submit']")).click();
+    void tc01() {
+        Login loginPage = new Login(driver);
+        loginPage.fillFormLogin("tomsmith", "SuperSecretPassword!");
     }
+
     @AfterMethod
-    void afterMethod(){
+    void afterMethod() {
         driver.quit();
     }
 }
