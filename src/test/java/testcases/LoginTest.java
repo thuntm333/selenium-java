4f1fbc6962924f14385760f08b28d030f7f6f59b
thuntm333 <mthu.nguyen394@gmail.com>
2020-10-10 10:18:46 +0700

    initial commit

---
+++ src/test/java/testcases/LoginTest.java
@@ -0,0 +1,28 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    void beforeMethod(){
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/login");
+    }
+    @Test
+    void tc01(){
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+    }
+    @AfterMethod
+    void afterMethod(){
+        driver.quit();
+    }
+}
